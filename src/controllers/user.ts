f6f35b008aa1b3e41d7254df6c805718d4f8c47b
jump-and-jump <984292420@qq.com>
2025-01-27 01:53:25 +0800

    feat: add Dockerfile

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,4 +1,4 @@
-import jwt from "@elysiajs/jwt";
+import { jwt } from "@elysiajs/jwt";
 import { Elysia } from "elysia";
 import { JWT_NAME } from "../config/constant";
 import { useSqlite } from "../lib/db";
@@ -7,13 +7,6 @@ import { UserService } from "../services/user";
 
 export const userController = new Elysia({ prefix: "/user" })
   .decorate("userService", new UserService({ db: useSqlite() }))
-  .use(
-    jwt({
-      name: JWT_NAME,
-      secret: "todo",
-      exp: "12h",
-    }),
-  )
   .get("/login", async ({ cookie: { accessToken }, jwt }) => {
     const value = await jwt.sign({ sub: "1" });
     accessToken.set({
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
+import jwt from "@elysiajs/jwt";
 import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
+import { JWT_NAME } from "./config/constant";
 import { userController } from "./controllers/user";
 
-async function bootstrap() {
-  const app = new Elysia().use(swagger()).use(userController).listen(3000);
+const isProduction = Bun.env.NODE_ENV === "production";
 
+function bootstrap() {
+  const app = new Elysia();
+
+  if (!isProduction) {
+    app.use(swagger());
+  }
+
+  app.use(
+    jwt({
+      name: JWT_NAME,
+      secret: "todo",
+      exp: "12h",
+      //  Bun.env.JWT_SECRET!,
+    }),
+  );
+
+  app.group("/api", (app) => app.use(userController)).listen(3000);
   console.log(
     `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
   );
+  return app;
 }
 
 bootstrap();
